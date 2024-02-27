46ba5a88274ecb22fbff4bef9383474959808d82
Andreye <drontop1999@mail>
2024-02-27 20:51:07 +0300

    Renaming

--- wsgi.py
+++ wsgi.py
@@ -4,3 +4,6 @@ from Lesson_3.Semminar_3.appwithforms import app2
 if __name__ == '__main__':
     # app.run(host='127.0.0.1', port=5000, debug=True)
     app2.run(host='127.0.0.1', port=5000, debug=True)
+
+
+
